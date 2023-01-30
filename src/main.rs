e6c011a6c580b5b1102404df08334bd4166253ec
Jeremy Zelaya <jzelaya@krames.com>
2023-01-30 13:32:44 -0600

    feat: pick settings from yaml file
    Allow users to pick settings from a yaml file. This is a breaking change

--- src/main.rs
+++ src/main.rs
@@ -9,31 +9,66 @@ use config::Config;
 
 fn main() {
     println!("Environment Builder");
-    // Read toml file's settings
+    // Read settings
     let settings = Config::builder()
-        .add_source(config::File::with_name("settings"))
+        .add_source(config::File::with_name("settings.yaml"))
         .build()
         .unwrap();
-    let path: String = settings.get("path").unwrap();
+    // Print paths
+    let paths: Vec<String> = settings.get("paths").unwrap();
+    // Print items inside paths vec
+    for (i, path) in paths.iter().enumerate() {
+        println!("{}: {}", i, path);
+    }    
+    // Get user input
+    let mut input = String::new();
+    println!("Please enter a number:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error reading number");
+    let input = input.trim();
+    // Convert input to usize
+    let input: usize = input.parse().unwrap();
+    // Print selected path
+    println!("Selected path: {}", paths[input]);
+
     // Change directories
-    let root_path = Path::new(&path);
+    let root_path = Path::new(&paths[input]);
     env::set_current_dir(&root_path).expect("An error ocurred while changing directories");
 
     // Get ticket key
-    let mut key = String::new();
-    println!("Please enter a JIRA ticket key:");
+    let keys: Vec<String> = settings.get("keys").unwrap();
+    // Print items inside keys vec
+    for (i, key) in keys.iter().enumerate() {
+        println!("{}: {}", i, key);
+    }
+    // Get user input
+    let mut input = String::new();
+    println!("Please enter a number:");
     io::stdin()
-        .read_line(&mut key)
-        .expect("Error reading JIRA ticket key");
-    let key = key.trim();
+        .read_line(&mut input)
+        .expect("Error reading number");
+    let input = input.trim();
+    // Convert input to usize
+    let input: usize = input.parse().unwrap();
+    // Print selected key
+    println!("Selected key: {}", keys[input]);
+
+    // Get a ticket number
+    let mut ticket = String::new();
+    println!("Please enter a ticket number:");
+    io::stdin()
+        .read_line(&mut ticket)
+        .expect("Error reading ticket number");
+    let ticket = ticket.trim();
 
     // Run worktree
     let worktree_command = Command::new("git")
         .arg("worktree")
         .arg("add")
-        .arg(key)
+        .arg(format!("{}-{}", &keys[input], ticket))
         .arg("-b")
-        .arg(format!("feature/{}", key))
+        .arg(format!("feature/{}", keys[input]))
         .status()
         .expect("An error ocurred while running worktree add");
     if worktree_command.success() {
