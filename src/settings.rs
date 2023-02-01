fe85421c8915aae1683765243574ba97cb56333d
Jeremy Zelaya <jzelaya@krames.com>
2023-02-01 14:13:50 -0600

    misc: group into struct

--- src/settings.rs
+++ src/settings.rs
@@ -5,49 +5,58 @@ use std::{
     io::{self, Write},
 };
 
-pub fn get_property(property: &str, config: &Config) -> String {
-    let option: String = config.get(property).unwrap();
-    return option;
+pub struct Settings {
+    configuration: Config,
 }
 
-pub fn get_property_list(property: &str, config: &Config) -> String {
-    let options: Vec<String> = config.get(property).unwrap();
-    for (i, path) in options.iter().enumerate() {
-        println!("{}: {}", i, path);
+impl Settings {
+    pub fn new(&self) -> Settings {
+        let config = self.read();
+        Settings {
+            configuration: config,
+        }
     }
-    // Get user input
-    let mut input = String::new();
-    print!("Please pick an option: ");
-    io::stdout().flush().unwrap();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Error reading number");
-    let input = input.trim();
-    // Convert input to usize
-    let input: usize = input.parse().unwrap();
-    return options[input].clone();
-}
-
-pub fn get_settings() -> Config {
-    // Check OS and set path
-    let mut settings_path = String::new();
-    let home_dir = dirs::home_dir().unwrap();
-    let os = env::consts::OS;
-    if os == "windows" {
-        settings_path = format!(
-            "{}\\.environment-builder\\settings.yaml",
-            home_dir.to_str().unwrap()
-        );
-    } else {
-        settings_path = format!(
-            "{}/environment-builder/settings.yaml",
-            home_dir.to_string_lossy()
-        );
+    fn read(&self) -> Config {
+        // Check OS and set path
+        let mut settings_path = String::new();
+        let home_dir = dirs::home_dir().unwrap();
+        let os = env::consts::OS;
+        if os == "windows" {
+            settings_path = format!(
+                "{}\\.environment-builder\\settings.yaml",
+                home_dir.to_str().unwrap()
+            );
+        } else {
+            settings_path = format!(
+                "{}/environment-builder/settings.yaml",
+                home_dir.to_string_lossy()
+            );
+        }
+        let config = Config::builder()
+            .add_source(config::File::with_name(&settings_path))
+            .build()
+            .unwrap();
+        return config;
     }
-    let settings = Config::builder()
-        .add_source(config::File::with_name(&settings_path))
-        .build()
-        .unwrap();
-    return settings;
+    // TODO figure out if configuration.get must always return a string
+    pub fn get(&self, name: &str) -> String {
+        // This is deprecated! https://docs.rs/config/latest/config/struct.Config.html#method.get
+        let options: Vec<String> = self.configuration.get(name).unwrap();
+        for (i, path) in options.iter().enumerate() {
+            println!("{}: {}", i, path);
+        }
+        // Get user input
+        let mut input = String::new();
+        print!("Please pick an option: ");
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading number");
+        let input = input.trim();
+        // Convert input to usize
+        let input: usize = input.parse().unwrap();
+        return options[input].clone();
+    }
 }
