6af640201775be21328d3a655eb305ff69b06c0d
Jeremy Zelaya <jzelaya@krames.com>
2023-02-02 14:02:04 -0600

    feat: add support for other OS

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,5 @@
 use colored::Colorize;
-use std::process::Command;
+use std::{process::Command, env};
 
 use crate::jira::JiraTicket;
 
@@ -19,12 +19,21 @@ pub fn add_worktree(ticket: &JiraTicket) {
     }
 }
 
+fn vs_code_command() -> String {
+    let os = env::consts::OS;
+    if os == "windows" {
+        return "code.cmd".to_string();
+    } else {
+        return "code".to_string();
+    } 
+}
+
 pub fn open_vscode(ticket: Option<&JiraTicket>) {
     let path = match ticket {
         Some(path) => path.to_string(),
         None => ".".to_string(),
     };
-        let vscode_command = Command::new("code.cmd")
+        let vscode_command = Command::new(vs_code_command())
         .arg(path)
         .status()
         .expect("An error ocurred while opening vs code");
